3049494f511d1b73853bda86e18820415961fb7f
02arslon <arslonruziboyev@gmail.com>
2023-08-01 16:36:07 +0900

    changed

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <section className="hero-wrapper">
+        <div className="flexCenter paddings innerWidth hero-container"></div>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
