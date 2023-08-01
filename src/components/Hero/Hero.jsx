b9aac88a8cdc349e16d5bc5f35748fa28a513be0
02arslon <arslonruziboyev@gmail.com>
2023-08-01 17:16:43 +0900

    changed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import HeroImg from "../../assets/hero-image.png";
+import { HiLocationMarker } from "react-icons/hi";
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <div>
       <section className="hero-wrapper">
-        <div className="flexCenter paddings innerWidth hero-container"></div>
+        <div className="flexCenter paddings innerWidth hero-container">
+          {/* Left Side of Header */}
+          <div className="flexColStart hero-left">
+            <div className="hero-title">
+              <div className="circle-title" />
+              <h1>
+                Discover <br /> Most Suitable <br /> Proberty
+              </h1>
+            </div>
+            <div className="flexColStart hero-descp">
+              <span>
+                Find a variety of properties that suit you very easilty
+              </span>
+              <span>
+                Forget all difficulties in finding a residence for you
+              </span>
+            </div>
+            <div className="flexCenter search-bar">
+              <HiLocationMarker color="var(--blue)" size={25} />
+              <input type="text" />
+              <button className="button">Search</button>
+            </div>
+          </div>
+          {/* Rught Side of Header*/}
+          <div className="flexCenter hero-right">
+            <div className="image-container">
+              <img src={HeroImg} alt="Hero Image" />
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
