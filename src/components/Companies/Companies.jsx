556939c0694d22fdfd370b2dc50a77f2141e81d2
02arslon <arslonruziboyev@gmail.com>
2023-08-03 18:20:18 +0900

    changed

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { ImageData } from "./data";
 import "./Companies.css";
 
 function Companies() {
-  return <div>Comp</div>;
+  return (
+    <section className="c-wrapper">
+      <div className="flexCenter paddings innerWidth c-container">
+        {ImageData.map((props, id) => (
+          <img src={props.image} alt={props.title} key={id} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Companies;
---
+++ src/components/Companies/data.js
@@ -0,0 +1,23 @@
+import Img1 from "../../assets/prologis.png";
+import Img2 from "../../assets/tower.png";
+import Img3 from "../../assets/equinix.png";
+import Img4 from "../../assets/realty.png";
+
+export const ImageData = [
+  {
+    image: Img1,
+    title: "prologis",
+  },
+  {
+    image: Img2,
+    title: "Tower",
+  },
+  {
+    image: Img3,
+    title: "Equinx",
+  },
+  {
+    image: Img4,
+    title: "Realty",
+  },
+];
