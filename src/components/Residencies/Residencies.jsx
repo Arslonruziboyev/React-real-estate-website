9786846a0a115ef169312e83d60023cee70fca89
02arslon <arslonruziboyev@gmail.com>
2023-08-04 17:43:15 +0900

    changed

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
 import "./Residencies.css";
+import data from "../../utils/slider.json";
 
 function Residencies() {
-  return <div>Residencies</div>;
+  return (
+    <section className="r-wrapper">
+      <div className="flexCenter paddings innerWidt r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular Residencies</span>
+        </div>
+        <Swiper>
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="r-card">
+                <img src={card.image} alt="Home" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 }
 
 export default Residencies;
