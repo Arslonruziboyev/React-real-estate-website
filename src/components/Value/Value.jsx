c78258c590a3dfc32f82ba5796be26678f3c99d3
02arslon <arslonruziboyev@gmail.com>
2023-08-05 18:38:32 +0900

    changed

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Value.css";
+import valuePng from "../../assets/value.png";
 import {
   Accordion,
   AccordionItem,
@@ -8,9 +9,22 @@ import {
   AccordionItemPanel,
   AccordionItemState,
 } from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
 
 function Value() {
-  return <div>Value</div>;
+  return (
+    <section className="v-wrapper">
+      <div className="flexCenter paddings innerWidth v-container">
+        {/* Left Side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src={valuePng} alt="Valus PNG" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Value;
