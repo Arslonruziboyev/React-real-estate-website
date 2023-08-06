6648e056edd9764196cfa001a0ec33f7097609c4
02arslon <arslonruziboyev@gmail.com>
2023-08-06 11:35:07 +0900

    changed

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <section className="c-wrapper">
+      <div className="flexCenter paddings innerWidth c-container">
+        {/* Left Side */}
+        <div className="c-left">Left</div>
+        {/* Right Side */}
+        <div className="c-right">
+          <div className="image-container">Image</div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
