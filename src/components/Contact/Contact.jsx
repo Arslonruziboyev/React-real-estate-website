f00c5f60ca118f8ece280bd857c8773909d26bf8
02arslon <arslonruziboyev@gmail.com>
2023-08-08 17:22:08 +0900

    changed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ContctImg from "../../assets/contact.jpg";
 import "./Contact.css";
 
 function Contact() {
@@ -9,7 +10,9 @@ function Contact() {
         <div className="c-left">Left</div>
         {/* Right Side */}
         <div className="c-right">
-          <div className="image-container">Image</div>
+          <div className="image-container">
+            <img src={ContctImg} alt="Contact Image" />
+          </div>
         </div>
       </div>
     </section>
