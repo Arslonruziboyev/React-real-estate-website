63f63c180a8f07084d482c60ef771fbd38aea671
02arslon <arslonruziboyev@gmail.com>
2023-08-08 17:24:24 +0900

    changed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,14 @@ function Contact() {
     <section className="c-wrapper">
       <div className="flexCenter paddings innerWidth c-container">
         {/* Left Side */}
-        <div className="c-left">Left</div>
+        <div className="c-left">
+          <span className="orangeText">Our Contacts</span>
+          <span className="primaryText">Easy to contact us</span>
+          <span className="secondaryText">
+            We always ready to help by providijng the best services for you. We
+            beleive a good blace to live can make your life better
+          </span>
+        </div>
         {/* Right Side */}
         <div className="c-right">
           <div className="image-container">
