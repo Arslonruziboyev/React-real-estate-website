3092852efd3acaebf6692afd7fa4351f0cb6392c
02arslon <arslonruziboyev@gmail.com>
2023-08-09 18:34:16 +0900

    changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Logo from "../../assets/logo2.png";
 import "./Footer.css";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <section className="f-wrapper">
+      <div className="flexCenter paddings innerWidth f-container">
+        {/* Left Side */}
+
+        <div className="flexColStart g-left">
+          <img src={Logo} alt="Logo" width={120} />
+
+          <span className="secondaryText">
+            Our vision is to make all people <br />
+            the best place to live for them.
+          </span>
+        </div>
+        <div className="flexColStart f-right"></div>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
