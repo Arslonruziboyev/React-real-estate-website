c9ee400bde08e614fcc5507c330cb0da601b3266
02arslon <arslonruziboyev@gmail.com>
2023-08-09 11:43:31 +0900

    changed

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -2,8 +2,22 @@ import React from "react";
 import "./GetStarted.css";
 
 function GetStarted() {
-  return <div>GetStarted</div>;
+  return (
+    <div>
+      <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+          <div className="flexColCenter inner-container">
+            <span className="primaryText">Get Started with Us</span>
+            <span className="secondaryText">
+              Subscribe and find super attractive price quotes from us.
+              <br /> Find your residence soon
+            </span>
+            <span></span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default GetStarted;
-GetStarted;
